bc205c1fe94cf668cd096fb54b2026ca9ca14e88
markitosha <markitosha@gmail.com>
2025-06-16 15:30:44 +0200

    Extended main client with environment parameter

---
+++ src/ExtendedClient.ts
@@ -0,0 +1,14 @@
+import { CortiClient as FernClient } from "./Client";
+
+type Options = Omit<FernClient.Options, 'environment'> & {
+    environment: 'beta-eu' | 'us' | 'eu' | string;
+};
+
+export class CortiClient extends FernClient {
+    constructor(options: Options) {
+        super({
+            ...options,
+            baseUrl: `https://api.${options.environment}.corti.app/v2`
+        });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * as Corti from "./api/index.js";
-export { CortiClient } from "./Client.js";
+export { CortiClient } from "./ExtendedClient.js";
 export { CortiEnvironment } from "./environments.js";
 export { CortiError, CortiTimeoutError } from "./errors/index.js";
 export * as serialization from "./serialization/index.js";
