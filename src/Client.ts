25277dcabd6d6d00dc62dbe90db5c12e1b5c9a9a
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-06-16 13:09:14 +0000

    SDK regeneration

--- src/Client.ts
+++ src/Client.ts
@@ -4,6 +4,7 @@
 
 import * as environments from "./environments.js";
 import * as core from "./core/index.js";
+import { Auth } from "./api/resources/auth/client/Client.js";
 import { mergeHeaders } from "./core/headers.js";
 import { Interactions } from "./api/resources/interactions/client/Client.js";
 import { Recordings } from "./api/resources/recordings/client/Client.js";
@@ -13,9 +14,10 @@ export declare namespace CortiClient {
         environment: core.Supplier<environments.CortiEnvironment | string>;
         /** Specify a custom URL to connect the client to. */
         baseUrl?: core.Supplier<string>;
-        token: core.Supplier<core.BearerToken>;
+        clientId: core.Supplier<string>;
+        clientSecret: core.Supplier<string>;
         /** Override the Tenant-Name header */
-        tenantName: core.Supplier<string>;
+        tenantName?: core.Supplier<string | undefined>;
         /** Additional headers to include in requests. */
         headers?: Record<string, string | core.Supplier<string | undefined> | undefined>;
     }
@@ -28,7 +30,7 @@ export declare namespace CortiClient {
         /** A hook to abort the request. */
         abortSignal?: AbortSignal;
         /** Override the Tenant-Name header */
-        tenantName?: string;
+        tenantName?: string | undefined;
         /** Additional headers to include in the request. */
         headers?: Record<string, string | core.Supplier<string | undefined> | undefined>;
     }
@@ -36,8 +38,10 @@ export declare namespace CortiClient {
 
 export class CortiClient {
     protected readonly _options: CortiClient.Options;
+    private readonly _oauthTokenProvider: core.OAuthTokenProvider;
     protected _interactions: Interactions | undefined;
     protected _recordings: Recordings | undefined;
+    protected _auth: Auth | undefined;
 
     constructor(_options: CortiClient.Options) {
         this._options = {
@@ -55,10 +59,28 @@ export class CortiClient {
                 _options?.headers,
             ),
         };
+
+        this._oauthTokenProvider = new core.OAuthTokenProvider({
+            clientId: this._options.clientId,
+            clientSecret: this._options.clientSecret,
+            authClient: new Auth({
+                environment: this._options.environment,
+            }),
+        });
     }
 
     public get interactions(): Interactions {
-        return (this._interactions ??= new Interactions(this._options));
+        return (this._interactions ??= new Interactions({
+            ...this._options,
+            token: async () => await this._oauthTokenProvider.getToken(),
+        }));
+    }
+
+    public get auth(): Auth {
+        return (this._auth ??= new Auth({
+            ...this._options,
+            token: async () => await this._oauthTokenProvider.getToken(),
+        }));
     }
 
     public get recordings(): Recordings {
