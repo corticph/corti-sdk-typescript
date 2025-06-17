7724c08c4595ecd836126f7767657b7ca9818ee3
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-06-17 13:05:59 +0000

    SDK regeneration

--- src/api/resources/index.ts
+++ src/api/resources/index.ts
@@ -2,5 +2,6 @@ export * as interactions from "./interactions/index.js";
 export * from "./interactions/types/index.js";
 export * as auth from "./auth/index.js";
 export * from "./auth/types/index.js";
+export * as recordings from "./recordings/index.js";
 export * from "./interactions/client/requests/index.js";
 export * from "./auth/client/requests/index.js";
