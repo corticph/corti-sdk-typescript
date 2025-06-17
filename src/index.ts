660ed4215b539c0393847a81be4e731fec8c1215
markitosha <markitosha@gmail.com>
2025-06-17 12:15:01 +0200

    Exported Auth Client separately to have an access to authentification methods

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { CortiClient } from "./ExtendedClient.js";
 export { CortiEnvironment } from "./environments.js";
 export { CortiError, CortiTimeoutError } from "./errors/index.js";
 export * as serialization from "./serialization/index.js";
+export { Auth as CortiAuth } from "./api/resources/auth/client/ExtendedClient.js";
