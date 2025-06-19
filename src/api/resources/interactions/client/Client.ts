8974f4abe4753278d7cb741575e57eff854ce8ef
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-06-19 14:44:28 +0000

    SDK regeneration

--- src/api/resources/interactions/client/Client.ts
+++ src/api/resources/interactions/client/Client.ts
@@ -15,7 +15,7 @@ export declare namespace Interactions {
         environment: core.Supplier<environments.CortiEnvironment | string>;
         /** Specify a custom URL to connect the client to. */
         baseUrl?: core.Supplier<string>;
-        token: core.Supplier<core.BearerToken>;
+        token?: core.Supplier<core.BearerToken | undefined>;
         /** Override the Tenant-Name header */
         tenantName: core.Supplier<string>;
         /** Additional headers to include in requests. */
@@ -632,7 +632,12 @@ export class Interactions {
         }
     }
 
-    protected async _getAuthorizationHeader(): Promise<string> {
-        return `Bearer ${await core.Supplier.get(this._options.token)}`;
+    protected async _getAuthorizationHeader(): Promise<string | undefined> {
+        const bearer = await core.Supplier.get(this._options.token);
+        if (bearer != null) {
+            return `Bearer ${bearer}`;
+        }
+
+        return undefined;
     }
 }
