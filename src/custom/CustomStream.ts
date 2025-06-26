75ab51dc1e16962851fa826adcc7f2e1c3b76f25
markitosha <markitosha@gmail.com>
2025-06-26 11:08:01 +0200

    Temporary removed custom stream implementation

--- src/custom/CustomStream.ts
+++ src/custom/CustomStream.ts
@@ -2,44 +2,44 @@
  * Patch: use custom Stream implementation to support passing _options parameters to connection function
  */
 import { Stream as FernStream } from "../api/resources/stream/client/Client.js";
-import * as core from "../core/index.js";
-import { StreamSocket } from "../api/resources/stream/client/Socket.js";
-import * as qs from "qs";
+// import * as core from "../core/index.js";
+// import { StreamSocket } from "../api/resources/stream/client/Socket.js";
+// import * as qs from "qs";
 
 export class Stream extends FernStream {
-    public async connect(args: Omit<FernStream.ConnectArgs, 'token' | 'tenant-name' | 'tenantName'>): Promise<StreamSocket> {
-        return this.__connect_patch({
-            ...args,
-            token: await this._getAuthorizationHeader(),
-            "tenant-name": await core.Supplier.get(this._options.tenantName),
-            tenantName: await core.Supplier.get(this._options.tenantName),
-        });
-    }
-
-    /**
-     * Patch: temporary fix of WS URL generation, while it doesn't work from Ferns side
-     */
-    private async __connect_patch(args: FernStream.ConnectArgs): Promise<StreamSocket> {
-        const queryParams: Record<string, unknown> = {};
-        if (args["token"] != null) {
-            queryParams["token"] = args["token"];
-        }
-
-        if (args["tenant-name"] != null) {
-            queryParams["tenant-name"] = args["tenant-name"];
-        }
-
-        let websocketHeaders: Record<string, unknown> = {};
-        websocketHeaders = {
-            ...websocketHeaders,
-            ...args["headers"],
-        };
-        const socket = new core.ReconnectingWebSocket(
-            `${(await core.Supplier.get(this._options["baseUrl"])) ?? (await core.Supplier.get(this._options["environment"])).wss}/audio-bridge/v2/interactions/${encodeURIComponent(args["id"])}/streams?${qs.stringify(queryParams, { arrayFormat: "repeat" })}`,
-            [],
-            { debug: args["debug"] ?? false, maxRetries: args["reconnectAttempts"] ?? 30 },
-            websocketHeaders,
-        );
-        return new StreamSocket({ socket });
-    }
+    // public async connect(args: Omit<FernStream.ConnectArgs, 'token' | 'tenant-name' | 'tenantName'>): Promise<StreamSocket> {
+    //     return this.__connect_patch({
+    //         ...args,
+    //         token: await this._getAuthorizationHeader(),
+    //         "tenant-name": await core.Supplier.get(this._options.tenantName),
+    //         tenantName: await core.Supplier.get(this._options.tenantName),
+    //     });
+    // }
+    //
+    // /**
+    //  * Patch: temporary fix of WS URL generation, while it doesn't work from Ferns side
+    //  */
+    // private async __connect_patch(args: FernStream.ConnectArgs): Promise<StreamSocket> {
+    //     const queryParams: Record<string, unknown> = {};
+    //     if (args["token"] != null) {
+    //         queryParams["token"] = args["token"];
+    //     }
+    //
+    //     if (args["tenant-name"] != null) {
+    //         queryParams["tenant-name"] = args["tenant-name"];
+    //     }
+    //
+    //     let websocketHeaders: Record<string, unknown> = {};
+    //     websocketHeaders = {
+    //         ...websocketHeaders,
+    //         ...args["headers"],
+    //     };
+    //     const socket = new core.ReconnectingWebSocket(
+    //         `${(await core.Supplier.get(this._options["baseUrl"])) ?? (await core.Supplier.get(this._options["environment"])).wss}/audio-bridge/v2/interactions/${encodeURIComponent(args["id"])}/streams?${qs.stringify(queryParams, { arrayFormat: "repeat" })}`,
+    //         [],
+    //         { debug: args["debug"] ?? false, maxRetries: args["reconnectAttempts"] ?? 30 },
+    //         websocketHeaders,
+    //     );
+    //     return new StreamSocket({ socket });
+    // }
 }
