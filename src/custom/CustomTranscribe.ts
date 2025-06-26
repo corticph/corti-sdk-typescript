a47740734910f1ada5e5a0b79c7fc2396e25b6b1
markitosha <markitosha@gmail.com>
2025-06-26 14:55:01 +0200

    Added patches to WebSocket implementations

--- src/custom/CustomTranscribe.ts
+++ src/custom/CustomTranscribe.ts
@@ -1,45 +1,16 @@
 /**
- * Patch: use custom Stream implementation to support passing _options parameters to connection function
+ * Patch: use custom Transcribe implementation to support passing _options parameters to connection function
  */
 import { Transcribe as FernTranscribe } from "../api/resources/transcribe/client/Client.js";
-// import * as core from "../core/index.js";
-// import { StreamSocket } from "../api/resources/stream/client/Socket.js";
-// import * as qs from "qs";
+import * as core from "../core/index.js";
+import { TranscribeSocket } from "../api/resources/transcribe/client/Socket.js";
 
 export class Transcribe extends FernTranscribe {
-    // public async connect(args: Omit<FernStream.ConnectArgs, 'token' | 'tenant-name' | 'tenantName'>): Promise<StreamSocket> {
-    //     return this.__connect_patch({
-    //         ...args,
-    //         token: await this._getAuthorizationHeader(),
-    //         "tenant-name": await core.Supplier.get(this._options.tenantName),
-    //         tenantName: await core.Supplier.get(this._options.tenantName),
-    //     });
-    // }
-    //
-    // /**
-    //  * Patch: temporary fix of WS URL generation, while it doesn't work from Ferns side
-    //  */
-    // private async __connect_patch(args: FernStream.ConnectArgs): Promise<StreamSocket> {
-    //     const queryParams: Record<string, unknown> = {};
-    //     if (args["token"] != null) {
-    //         queryParams["token"] = args["token"];
-    //     }
-    //
-    //     if (args["tenant-name"] != null) {
-    //         queryParams["tenant-name"] = args["tenant-name"];
-    //     }
-    //
-    //     let websocketHeaders: Record<string, unknown> = {};
-    //     websocketHeaders = {
-    //         ...websocketHeaders,
-    //         ...args["headers"],
-    //     };
-    //     const socket = new core.ReconnectingWebSocket(
-    //         `${(await core.Supplier.get(this._options["baseUrl"])) ?? (await core.Supplier.get(this._options["environment"])).wss}/audio-bridge/v2/interactions/${encodeURIComponent(args["id"])}/streams?${qs.stringify(queryParams, { arrayFormat: "repeat" })}`,
-    //         [],
-    //         { debug: args["debug"] ?? false, maxRetries: args["reconnectAttempts"] ?? 30 },
-    //         websocketHeaders,
-    //     );
-    //     return new StreamSocket({ socket });
-    // }
+    public async connect(args: Omit<FernTranscribe.ConnectArgs, 'token' | 'tenant-name'>): Promise<TranscribeSocket> {
+        return super.connect({
+            ...args,
+            token: await this._getAuthorizationHeader(),
+            "tenant-name": await core.Supplier.get(this._options.tenantName),
+        });
+    }
 }
