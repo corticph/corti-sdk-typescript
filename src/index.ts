877dcd742dd55cbaad2a6d43f16f6e589350fd6d
markitosha <markitosha@gmail.com>
2025-07-04 17:43:36 +0200

    Removed some Fern ignored files

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 export * as Corti from "./api/index.js";
+export { CortiError, CortiTimeoutError } from "./errors/index.js";
+export * as serialization from "./serialization/index.js";
 /**
  * Patch: use custom CortiClient instead of the generated one.
  */
 export { CortiClient } from "./custom/CortiClient.js";
 export { CortiEnvironment, CortiEnvironmentUrls } from "./environments.js";
-export { CortiError, CortiTimeoutError } from "./errors/index.js";
-/**
- * TEMP: removed type validation for Alpha 1
- */
-// export * as serialization from "./serialization/index.js";
 
 /**
  * Patch: added new export to provide Authorization code flow support.
