8c3bbc91e99ab0cf94ea3553521db03754138b6e
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-07 13:15:24 +0000

    SDK regeneration

--- src/api/types/TranscribeCommandData.ts
+++ src/api/types/TranscribeCommandData.ts
@@ -6,7 +6,7 @@ export interface TranscribeCommandData {
     /** To identify the command when it gets detected and returned over the WebSocket */
     id: string;
     /** The variables identified */
-    variables?: Record<string, unknown>;
+    variables?: Record<string, (string | null) | undefined> | null;
     /** The raw transcript without spoken punctuation applied and without command phrases removed */
     rawTranscriptText: string;
     /** Start time of the transcript segment in seconds */
